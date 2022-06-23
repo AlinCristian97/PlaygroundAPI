893b2dba79c11402f6d5b032a816027861262994
Alin Apostol <alin.cristian977@gmail.com>
2022-06-23 17:02:20 +0300

    added, implemented and integrated the file upload controller

---
+++ Controllers/FileUploadController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace PlaygroundAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FileUploadController : ControllerBase
+    {
+        [HttpPost, DisableRequestSizeLimit]
+        IActionResult Upload()
+        {
+            try
+            {
+                // extract the file from the request
+                var file = Request.Form.Files[0];
+
+                var folderName = "Resources";
+                var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+
+                if (file.Length > 0)
+                {
+                    var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName?.Trim('"');
+                    var fullPath = Path.Combine(pathToSave, fileName);
+
+                    using var stream = new FileStream(fullPath, FileMode.Create);
+                    file.CopyTo(stream);
+
+                    return Ok();
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex}");
+            }
+        }
+
+    }
+}
